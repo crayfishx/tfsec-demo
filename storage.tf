b03f1cf4db56b1d61dfcb89e322c00a546687827
Craig Dunn <craig@craigdunn.org>
2023-10-04 11:44:55 +0200

    initial commit

---
+++ storage.tf
@@ -0,0 +1,17 @@
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "tfsecdemo-test-bucket"
+}
+
+resource "aws_s3_bucket_ownership_controls" "bucket_acl" {
+  bucket = aws_s3_bucket.demo_bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket_acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.bucket_acl]
+
+  bucket = aws_s3_bucket.demo_bucket.id
+  acl    = "private-read"
+}
